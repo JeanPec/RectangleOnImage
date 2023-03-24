10ad7760fc574c6b644381f8f9059ce58f5a6c86
Sylvain <Sylvain@DESKTOP-8FM08F8>
2023-03-23 22:06:04 -0700

    Add project files.

---
+++ ProjectSylvainDerory/MainWindow.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
+using Point = System.Windows.Point;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace ProjectSylvainDerory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Point startingPoint;
+        Point offset;
+        Rectangle activeRectangle;
+        Rectangle movingRectangle;
+        String state_Cursor = "Add Rectangle";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // function used to check if the mouse is inside the image
+        // we use the function TranslatePoint to get the point relative to the Canvas Container
+        private Boolean Check_mouse_in_image(Point mouse)
+        {
+            if (imagePictures.Source != null)
+            {
+                Point imgPoint = imagePictures.TranslatePoint(new Point(0, 0), canvasImage);
+                if (mouse.X >= imgPoint.X && mouse.X < imgPoint.X + imagePictures.ActualWidth && mouse.Y >= 0 && mouse.Y < imgPoint.Y + imagePictures.ActualHeight)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Function used to convert the Color from the Rectangle
+        //to the type of color used by Color Picker
+        private System.Drawing.Color Convert_Color_to_Brush(Color color)
+        {
+            return System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+
+        //Function used to convert the Color from the ColorPicker
+        //to the type of color used by the Rectangle
+        private SolidColorBrush Convert_Brush_to_Color(System.Drawing.Color color)
+        {
+            return new SolidColorBrush(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+
+        private void CanvasImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.OriginalSource is Rectangle)
+            {
+                if(state_Cursor == "Change Rectangle Color")
+                {
+                    Rectangle myRectangle = (e.OriginalSource as Rectangle);
+                    Color rectColor = (myRectangle.Fill as SolidColorBrush).Color;
+                    System.Drawing.Color dialogColor = Convert_Color_to_Brush(rectColor);
+                    ColorDialog colorDialog = new ColorDialog
+                    {
+                        Color = dialogColor,
+                    };
+                    try
+                    {
+                        if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        {
+                            myRectangle.Fill = Convert_Brush_to_Color(colorDialog.Color);
+                        }
+                    }
+                    catch
+                    {
+
+                        System.Windows.MessageBox.Show("Error occured While Changing the color", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else if (state_Cursor == "Move Rectangle")
+                {
+                    movingRectangle = (e.OriginalSource as Rectangle);
+                    // Get Offset to move the element
+                    offset = e.GetPosition(canvasImage);
+                    offset.X -= Canvas.GetLeft(movingRectangle);
+                    offset.Y -= Canvas.GetTop(movingRectangle);
+                }
+                else if (state_Cursor == "Delete Rectangle")
+                {
+                    canvasImage.Children.Remove(e.OriginalSource as Rectangle);
+                }
+            }
+            else if (state_Cursor == "Add Rectangle") {
+                startingPoint = e.GetPosition(canvasImage);
+                if (Check_mouse_in_image(startingPoint))
+                {
+                    activeRectangle = new Rectangle
+                    {
+                        Fill = System.Windows.Media.Brushes.Teal,
+                    };
+
+                    Canvas.SetLeft(activeRectangle, startingPoint.X);
+                    Canvas.SetTop(activeRectangle, startingPoint.Y);
+
+                    canvasImage.Children.Add(activeRectangle);
+                }
+            }
+        }
+
+        private void CanvasImage_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            // make sure that we are resizing a rectangle
+            if (activeRectangle != null)
+            {
+                //get current point and test that we are still in the image
+                Point currentPosition = e.GetPosition(canvasImage);
+                if (Check_mouse_in_image(currentPosition))
+                {
+                    //calculate the width and height of the resizing rectangle
+                    double width = Math.Abs(currentPosition.X - startingPoint.X);
+                    double height = Math.Abs(currentPosition.Y - startingPoint.Y);
+
+                    //apply the new dimention to the active rectangle
+                    activeRectangle.Width = width;
+                    activeRectangle.Height = height;
+
+                    //set the position of the resized rectangle
+                    Canvas.SetLeft(activeRectangle, Math.Min(startingPoint.X, currentPosition.X));
+                    Canvas.SetTop(activeRectangle, Math.Min(startingPoint.Y, currentPosition.Y));
+                }
+            }
+            // make sure that we are moving a rectangle
+            if (movingRectangle != null)
+            {
+                //get current point and test that we are still in the image
+                Point currentPosition = e.GetPosition(canvasImage);
+                currentPosition.X -= offset.X;
+                currentPosition.Y -= offset.Y;
+                //Check that the Recatngle is still inside the Image
+                if(Check_mouse_in_image(currentPosition) &&
+                    Check_mouse_in_image(new Point(currentPosition.X, currentPosition.Y + movingRectangle.ActualHeight)) &&
+                    Check_mouse_in_image(new Point(currentPosition.X + movingRectangle.ActualWidth, currentPosition.Y)) &&
+                    Check_mouse_in_image(new Point(currentPosition.X + movingRectangle.ActualWidth, currentPosition.Y + movingRectangle.ActualHeight)))
+                {
+                    //set the position of the resized rectangle
+                    Canvas.SetLeft(movingRectangle, currentPosition.X);
+                    Canvas.SetTop(movingRectangle, currentPosition.Y);
+                }
+            }
+        }
+
+        private void CanvasImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //Add the Adorner to the Rectangle
+            Point imgPoint = imagePictures.TranslatePoint(new Point(0, 0), canvasImage);
+            if(activeRectangle != null) AdornerLayer.GetAdornerLayer(canvasImage).Add(new RectangleAdoner(activeRectangle, startingPoint, imgPoint, imagePictures.ActualWidth, imagePictures.ActualHeight));
+            // Reset the current active Rectangle
+            activeRectangle = null;
+            // Reset the moving Rectangle
+            movingRectangle = null;
+        }
+
+        private void state_Checked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.RadioButton radio = sender as System.Windows.Controls.RadioButton;
+            state_Cursor = radio.Content != null ? radio.Content.ToString() : "Add Rectangle";
+            switch (state_Cursor)
+            {
+                case "Delete Rectangle":
+                    Mouse.OverrideCursor = System.Windows.Input.Cursors.No;
+                    break;
+                case "Move Rectangle":
+                    Mouse.OverrideCursor = System.Windows.Input.Cursors.SizeAll;
+                    break;
+                case "Change Rectangle Color":
+                    Mouse.OverrideCursor = System.Windows.Input.Cursors.Pen;
+                    break;
+                default:
+                    Mouse.OverrideCursor = System.Windows.Input.Cursors.Arrow;
+                    break;
+            }
+
+        }
+
+        private void Upload_Img_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                // Filter what file can be uploaded
+                openFileDialog.Filter = "Image files|*.bmp;*.jpg;*.img;*.png";
+                openFileDialog.FilterIndex = 1;
+                // Show Dialog to choose wich file to upload
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    // upload Image to Canvas and stretch to the dimension of the canvas
+                    imagePictures.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                    imagePictures.Stretch = Stretch.Uniform;
+                    imagePictures.Width = canvasImage.Width;
+                    imagePictures.Height = canvasImage.Height;
+                }
+            }
+            catch
+            {
+
+                System.Windows.MessageBox.Show("Error occured While Uploading an Image", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Download_Img_Click(object sender, RoutedEventArgs e)
+        {
+            //Render Canvas as a Bitmap
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(
+                (int)canvasImage.ActualWidth,
+                (int)canvasImage.ActualHeight,
+                96d,
+                96d,
+                PixelFormats.Pbgra32);
+            renderBitmap.Render(canvasImage);
+            try
+            {
+                //Use Open File Dialog to indicate the name and location of the file we cant to create
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //Create PNG encoder to save Bitmap to a file
+                    PngBitmapEncoder pngEncoder = new PngBitmapEncoder();
+                    pngEncoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+                    using (FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                    {
+                        pngEncoder.Save(fileStream);
+                    }
+                }
+            }
+            catch
+            {
+
+                System.Windows.MessageBox.Show("Error occured While Downloading an Image", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ ProjectSylvainDerory/RectangleAdoner.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ProjectSylvainDerory
+{
+    partial class RectangleAdoner : Adorner
+    {
+        VisualCollection RectangleAdornerVisuals;
+        Thumb thumbUL, thumbUM, thumbUR, thumbMR, thumbDR, thumbDM, thumbDL, thumbML;
+        bool isVisible;
+        Point myPoint;
+        int minimumSize = 10;
+        double minX, maxX, minY, maxY;
+        Cursor activeCursor;
+
+        public RectangleAdoner(UIElement adornedElement, Point startingPoint, Point imgPoint, double imgWidth, double imgHeight) : base(adornedElement)
+        {
+            //Initialize and add elements and their methods to manage the resize
+            RectangleAdornerVisuals = new VisualCollection(this);
+            SolidColorBrush transparent = Brushes.Transparent;
+            thumbUL = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbUM = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbUR = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbMR = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbDR = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbDM = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbDL = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+            thumbML = new Thumb() { Background = transparent, Height = 15, Width = 15 };
+
+            thumbUL.DragDelta += Move_Diagonal_Up_Left;
+            thumbUM.DragDelta += Move_Vertical_Up;
+            thumbUR.DragDelta += Move_Diagonal_Up_Right;
+            thumbMR.DragDelta += Move_Horizontal_Right;
+            thumbDR.DragDelta += Move_Diagonal_Down_Right;
+            thumbDM.DragDelta += Move_Vertical_Down;
+            thumbDL.DragDelta += Move_Diagonal_Down_Left;
+            thumbML.DragDelta += Move_Horizontal_Left;
+
+            RectangleAdornerVisuals.Add(thumbUL);
+            RectangleAdornerVisuals.Add(thumbUM);
+            RectangleAdornerVisuals.Add(thumbUR);
+            RectangleAdornerVisuals.Add(thumbMR);
+            RectangleAdornerVisuals.Add(thumbDR);
+            RectangleAdornerVisuals.Add(thumbDM);
+            RectangleAdornerVisuals.Add(thumbDL);
+            RectangleAdornerVisuals.Add(thumbML);
+
+            myPoint = startingPoint;
+            minX = imgPoint.X;
+            maxX = imgPoint.X + imgWidth;
+            minY = imgPoint.Y;
+            maxY = imgPoint.Y + imgHeight;
+        }
+
+        private bool Check_mouse_in_image(Point mouse)
+        {
+            if (mouse.X >= minX && mouse.X < maxX && mouse.Y >= minY && mouse.Y < maxY)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool Check_mouse_in_image_treshhold(Point mouse)
+        {
+            //Added a treshhold so that when you are at the treshold the elment is put back inside the image 
+            //with a minimum value otherwise it will not be accesible anymore
+            if (mouse.X >= minX - 1 && mouse.X < maxX + 1 && mouse.Y >= minY - 1 && mouse.Y < maxY + 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Move_Horizontal_Left(object sender, DragDeltaEventArgs e)
+        {
+            //manage the mouvement to the left and change the icon
+            //cannot go further than the image and if it reaches the
+            //other side it is 0 we do not manage negative values
+            //store the cursor before the resize to go back to it afterwards
+            if (Mouse.OverrideCursor != Cursors.SizeWE) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeWE;
+            var ele = (FrameworkElement)AdornedElement;
+            var newX = ele.Width - e.HorizontalChange < minimumSize ? myPoint.X : myPoint.X + e.HorizontalChange;
+            if (Check_mouse_in_image(new Point(newX,myPoint.Y)))
+            {
+                myPoint.X = newX;
+                ele.Width = ele.Width - e.HorizontalChange < minimumSize ? minimumSize : ele.Width - e.HorizontalChange;
+                Canvas.SetLeft(ele, myPoint.X);
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(newX, myPoint.Y)))
+            {
+                myPoint.X = myPoint.X + minimumSize;
+                ele.Width = ele.Width - minimumSize;
+                Canvas.SetLeft(ele, myPoint.X);
+            }
+        }
+
+        private void Move_Diagonal_Down_Left(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeNESW) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeNESW;
+            var ele = (FrameworkElement)AdornedElement;
+            var newX = ele.Width - e.HorizontalChange < minimumSize ? myPoint.X : myPoint.X + e.HorizontalChange;
+            var newY = (ele.Height - e.VerticalChange < minimumSize ? myPoint.Y : myPoint.Y + e.VerticalChange) + ele.Height;
+            if (Check_mouse_in_image(new Point(newX, newY)))
+            {
+                myPoint.X = newX;
+                ele.Height = ele.Height + e.VerticalChange < minimumSize ? minimumSize : ele.Height + e.VerticalChange;
+                ele.Width = ele.Width - e.HorizontalChange < minimumSize ? minimumSize : ele.Width - e.HorizontalChange;
+                Canvas.SetLeft(ele, myPoint.X);
+            }
+            else if(Check_mouse_in_image_treshhold(new Point(newX, newY))){
+                if (Check_mouse_in_image_treshhold(new Point(newX, myPoint.Y)))
+                {
+                    myPoint.X = myPoint.X + minimumSize;
+                    ele.Width = ele.Width - minimumSize;
+                    Canvas.SetLeft(ele, myPoint.X);
+                }
+                else
+                {
+                    ele.Height = ele.Height + minimumSize;
+                }
+            }
+        }
+
+        private void Move_Vertical_Down(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeNS) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeNS;
+            var ele = (FrameworkElement)AdornedElement;
+            var newY = (ele.Height - e.VerticalChange < minimumSize ? myPoint.Y : myPoint.Y + e.VerticalChange) + ele.Height;
+            if (Check_mouse_in_image(new Point(myPoint.X, newY)))
+            {
+                ele.Height = ele.Height + e.VerticalChange < minimumSize ? minimumSize : ele.Height + e.VerticalChange;
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(myPoint.X, newY)))
+            {
+                ele.Height = ele.Height + minimumSize;
+            }
+        }
+
+        private void Move_Diagonal_Down_Right(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeNWSE) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeNWSE;
+            var ele = (FrameworkElement)AdornedElement;
+            var newX = (ele.Width - e.HorizontalChange < minimumSize ? myPoint.X : myPoint.X + e.HorizontalChange) + ele.Width;
+            var newY = (ele.Height - e.VerticalChange < minimumSize ? myPoint.Y : myPoint.Y + e.VerticalChange) + ele.Height;
+            if (Check_mouse_in_image(new Point(newX, newY)))
+            {
+                ele.Height = ele.Height + e.VerticalChange < minimumSize ? minimumSize : ele.Height + e.VerticalChange;
+                ele.Width = ele.Width + e.HorizontalChange < minimumSize ? minimumSize : ele.Width + e.HorizontalChange;
+            }
+            else if(Check_mouse_in_image_treshhold(new Point(newX, newY)))
+            {
+                if (Check_mouse_in_image_treshhold(new Point(myPoint.X, newY)))
+                {
+                    ele.Height = ele.Height + minimumSize;
+                }
+                else
+                {
+                    ele.Width = ele.Width - minimumSize;
+                }
+            }
+
+        }
+
+        private void Move_Horizontal_Right(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeWE) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeWE;
+            var ele = (FrameworkElement)AdornedElement;
+            var newX = (ele.Width - e.HorizontalChange < minimumSize ? myPoint.X : myPoint.X + e.HorizontalChange) + ele.Width;
+            if (Check_mouse_in_image(new Point(newX, myPoint.Y)))
+            {
+                ele.Width = ele.Width + e.HorizontalChange < minimumSize ? minimumSize : ele.Width + e.HorizontalChange;
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(newX, myPoint.Y)))
+            {
+                ele.Width = ele.Width - minimumSize;
+            }
+        }
+
+        private void Move_Diagonal_Up_Right(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeNESW) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeNESW;
+            var ele = (FrameworkElement)AdornedElement;
+            var newX = (ele.Width - e.HorizontalChange < minimumSize ? myPoint.X : myPoint.X + e.HorizontalChange) + ele.Width;
+            var newY = ele.Height - e.VerticalChange < minimumSize ? myPoint.Y : myPoint.Y + e.VerticalChange;
+            if (Check_mouse_in_image(new Point(newX, newY)))
+            {
+                ele.Height = ele.Height - e.VerticalChange < minimumSize ? minimumSize : ele.Height - e.VerticalChange;
+                ele.Width = ele.Width + e.HorizontalChange < minimumSize ? minimumSize : ele.Width + e.HorizontalChange;
+                myPoint.Y = newY;
+                Canvas.SetTop(ele, myPoint.Y);
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(newX, newY)))
+            {
+                if (Check_mouse_in_image_treshhold(new Point(myPoint.X, newY)))
+                {
+                    ele.Height = ele.Height + minimumSize;
+                    myPoint.Y = myPoint.Y - minimumSize;
+                    Canvas.SetTop(ele, myPoint.Y);
+                }
+                else
+                {
+                    ele.Width = ele.Width - minimumSize;
+                }
+            }
+        }
+
+        private void Move_Vertical_Up(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeNS) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeNS;
+            var ele = (FrameworkElement)AdornedElement;
+            var newY = ele.Height - e.VerticalChange < minimumSize ? myPoint.Y : myPoint.Y + e.VerticalChange;
+            if (Check_mouse_in_image(new Point(myPoint.X, newY)))
+            {
+                ele.Height = ele.Height - e.VerticalChange < minimumSize ? minimumSize : ele.Height - e.VerticalChange;
+                myPoint.Y = newY;
+                Canvas.SetTop(ele, myPoint.Y);
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(myPoint.X, newY)))
+            {
+                ele.Height = ele.Height - minimumSize;
+                myPoint.Y = myPoint.Y + minimumSize;
+                Canvas.SetTop(ele, myPoint.Y);
+            }
+        }
+
+        private void Move_Diagonal_Up_Left(object sender, DragDeltaEventArgs e)
+        {
+            if (Mouse.OverrideCursor != Cursors.SizeNWSE) activeCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.SizeNWSE;
+            var ele = (FrameworkElement)AdornedElement;
+            var newX = ele.Width - e.HorizontalChange < minimumSize ? myPoint.X : myPoint.X + e.HorizontalChange;
+            var newY = ele.Height - e.VerticalChange < minimumSize ? myPoint.Y : myPoint.Y + e.VerticalChange;
+            if (Check_mouse_in_image(new Point(newX, newY)))
+            {
+                ele.Height = ele.Height - e.VerticalChange < minimumSize ? minimumSize : ele.Height - e.VerticalChange;
+                ele.Width = ele.Width - e.HorizontalChange < minimumSize ? minimumSize : ele.Width - e.HorizontalChange;
+                myPoint.X = newX;
+                myPoint.Y = newY;
+                Canvas.SetLeft(ele, myPoint.X);
+                Canvas.SetTop(ele, myPoint.Y);
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(myPoint.X, newY)))
+            {
+                ele.Height = ele.Height + minimumSize;
+                myPoint.Y = myPoint.Y - minimumSize;
+                Canvas.SetTop(ele, myPoint.Y);
+            }
+            else if (Check_mouse_in_image_treshhold(new Point(newX, myPoint.Y)))
+            {
+                myPoint.X = myPoint.X + minimumSize;
+                ele.Width = ele.Width - minimumSize;
+                Canvas.SetLeft(ele, myPoint.X);
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            isVisible = true;
+            InvalidateVisual();
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = activeCursor;
+            isVisible = false;
+            InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            if(isVisible)
+            {
+                Pen rectPen = new Pen(Brushes.Gray, 2);
+                rectPen.DashStyle = DashStyles.Dash;
+                drawingContext.DrawRectangle(null, rectPen, new Rect(-2.5, -2.5, AdornedElement.DesiredSize.Width + 5, AdornedElement.DesiredSize.Height + 5));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1),new Rect(-5, -5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect((AdornedElement.DesiredSize.Width / 2) - 5, -5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect(AdornedElement.DesiredSize.Width - 5, -5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect(-5, (AdornedElement.DesiredSize.Height / 2) - 5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect(AdornedElement.DesiredSize.Width - 5, (AdornedElement.DesiredSize.Height / 2) - 5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect(-5, AdornedElement.DesiredSize.Height - 5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect((AdornedElement.DesiredSize.Width / 2) - 5, AdornedElement.DesiredSize.Height - 5, 10, 10));
+                drawingContext.DrawRectangle(Brushes.LightGray, new Pen(Brushes.Gray, 1), new Rect(AdornedElement.DesiredSize.Width - 5, AdornedElement.DesiredSize.Height - 5, 10, 10));
+
+            }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return RectangleAdornerVisuals[index];
+        }
+
+        protected override int VisualChildrenCount => (RectangleAdornerVisuals != null) ? RectangleAdornerVisuals.Count : 0;
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            thumbUL.Arrange(new Rect(-5, -5, 15, 15));
+            thumbUM.Arrange(new Rect((AdornedElement.DesiredSize.Width / 2) - 5, -5, 15, 15));
+            thumbUR.Arrange(new Rect(AdornedElement.DesiredSize.Width - 5, -5, 15, 15));
+            thumbML.Arrange(new Rect(-5, (AdornedElement.DesiredSize.Height / 2) - 5, 15, 15));
+            thumbMR.Arrange(new Rect(AdornedElement.DesiredSize.Width - 5, (AdornedElement.DesiredSize.Height / 2) - 5, 15, 15));
+            thumbDL.Arrange(new Rect(-5, AdornedElement.DesiredSize.Height - 5, 15, 15));
+            thumbDM.Arrange(new Rect((AdornedElement.DesiredSize.Width / 2) - 5, AdornedElement.DesiredSize.Height - 5, 15, 15));
+            thumbDR.Arrange(new Rect(AdornedElement.DesiredSize.Width - 5, AdornedElement.DesiredSize.Height - 5, 15, 15));
+
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
